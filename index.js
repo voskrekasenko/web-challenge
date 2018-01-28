b39c051c2e77f1f8bd4ab5a930a20e65c98f3aad
Max <voskrekasenko@gmail.com>
2018-01-28 19:52:53 +0200

    refactoring

--- index.js
+++ index.js
@@ -1,51 +1,62 @@
 (function(){
 	"use strict"
 
+	var data = JSON.parse(getJson('data.json'));
 	var i, j;
-	var data = {
-		"scale": ["15.03.2016", "16.03.2016", "17.03.2016", "18.03.2016", "19.03.2016", "13.04.2016", "16.04.2016"],
-		"items": [
-			{
-				"title": "Setup",
-				"start": "15.03.2016",
-				"end": "16.03.2016",
-				"color": "#11C01A"
-			},
-			{
-				"title": "Development",
-				"start": "16.03.2016",
-				"end": "17.03.2016",
-				"color": "#39B0DA"
-			},
-			{
-				"title": "Testing",
-				"start": "17.03.2016",
-				"end": "19.03.2016",
-				"color": "#F8BB3C"
-			},
-			{
-				"title": "Production",
-				"start": "18.03.2016",
-				"end": "16.04.2016",
-				"color": "#FF00DD"
-			}
-		]
-	}
-	document.write("<table border=" + 1 + ">");
-	for(i = 0; i <= data.items.length; i++){
-		if(i>=1){
-			document.write("<tr align=right><td>" + data.items[i-1].title);
+	var itemLength = data.items.length, scaleLength = data.scale.length;
+
+	diagram(data, 'main');
+
+	// document.write("<table border=" + 1 + ">");
+	// for(i = 0; i <= itemLength; i++){
+	// 	if(i>=1){
+	// 		document.write("<tr align=right><td>" + data.items[i-1].title);
+	// 	}
+	// 	for(j = 0; j <= scaleLength; j++){
+	// 		if(i === 0){
+	// 			document.write("<td>" + (data.scale[j-1] || '') + "</td>");
+	// 		} else if(data.scale[j]){
+	// 			document.write("<td"+hasBackground(data.items[i-1], data.scale[j])+"></td>");
+	// 		}
+	// 	}
+	// 	document.write("</tr>");
+	// }
+	// document.write("</table>");
+
+// epic functions
+
+	function getJson(url) {
+		var xhr = new XMLHttpRequest();
+
+		xhr.open('GET', url, false);
+		xhr.send();
+
+		if (xhr.status != 200) {
+			return xhr.status + ': ' + xhr.statusText;
+		} else {
+			return  xhr.responseText;
 		}
-		for(j = 0; j <= data.scale.length; j++){
-			if(i === 0){
-				document.write("<td>" + (data.scale[j-1] || '') + "</td>");
-			} else if(data.scale[j]){
-				document.write("<td"+hasBackground(data.items[i-1], data.scale[j])+"></td>");
-			}
-		}
-		document.write("</tr>");
 	}
-	document.write("</table>");
+
+	function diagram(data, element) {
+		var options = {
+			class: "blue"
+		}
+		var mainElement = document.getElementById(element);
+		data.items.forEach(function(elemItem){
+			mainElement.appendChild(newElement());
+			console.log(elemItem);
+			data.scale.forEach(function(elemScale){
+				elemScale.appendChild(newElement());
+				console.log(elemScale);
+			})
+		})
+		function newElement(value){
+			var element = document.createElement('div');
+			return element;
+		}
+	}
+
 	function hasBackground(item, scale){
 		var dateScale = scale.split('.'),
 			itemStart = item.start.split('.'),
@@ -55,4 +66,5 @@
 		}
 		return '';
 	}
+
 }())
